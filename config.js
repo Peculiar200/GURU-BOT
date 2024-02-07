96261f58897443e319be006bb42c9b737a273e01
Peculiar200 <158743556+Peculiar200@users.noreply.github.com>
2024-02-07 13:39:06 -0500

    Update config.js

--- config.js
+++ config.js
@@ -7,16 +7,16 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['3127734850', 'DAVID', true],
+  ['9023540384', 'DAVID', true],
   ['6281255369012', 'Ash', false], 
   [''] 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['3127734850','32460248586'] 
-global.prems = ['3127734850', '32460248586', '919398758484']
-global.allowed = ['3127734850', '32460248586', '19152999993']
+global.mods = ['9023540384','32460248586'] 
+global.prems = ['9023540384', '32460248586', '919398758484']
+global.allowed = ['9023540384', '32460248586', '19152999993']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
